d1d7b2ba6e440dbd2ade28b246093ae1f72f5389
LiebeLee <27326628+LiebeLee@users.noreply.github.com>
2020-12-14 00:34:48 +0800

    Update sp.py
    update code

--- sp.py
+++ sp.py
@@ -1,7 +1,12 @@
 import os
 import time
 from selenium import webdriver
-import json
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import random
+import json 
+
 
 
 def browser_init(isWait):
@@ -11,7 +16,7 @@ def browser_init(isWait):
         'profile.default_content_setting_values.images': 2
     }
     options.add_experimental_option('prefs', prefs)
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(options=options)
     if isWait:
         browser.implicitly_wait(10)
     return browser
@@ -23,34 +28,87 @@ def openPage(browser):
 
 def collectTitleAndKeyword(browser, papers):
     # cont = browser.find_element_by_class_name('lplist')
+    retry_max = 5
+    wait = WebDriverWait(browser, 10, 0.5)
+    contentLocator = (By.XPATH, "//div[@class='lplist']/div[@class='list-item']")
+    try:
+        wait.until(EC.presence_of_element_located(contentLocator))
+    except:
+        print("collectTitleAndKeyword wait list-item time out, failed!")
+        return False
+    
     cont = browser.find_elements_by_xpath("//div[@class='lplist']/div[@class='list-item']")
     for _, item in enumerate(cont):
-        ti = item.find_element_by_xpath("p[@class='tit clearfix']/a[1]")
-        title = ti.get_attribute("title")
-        # print("titile:", title)
+        count1 = 0
+        flag1 = True
+        title = ""
+        while flag1 and count1 < retry_max:
+            try:
+                ti = item.find_element_by_xpath("p[@class='tit clearfix']/a[1]")
+                title = ti.get_attribute("title")
+                flag1 = False
+            except:
+                count1 += 1
+                time.sleep(1)
+        if flag1:
+            continue
 
         if title.find("互联网+") == -1 or title.find("教育") == -1:
             continue
 
-        keyInfo = item.find_element_by_xpath("div[@class='info']/p[@class='info_left left']")
-        keyElement = keyInfo.find_element_by_xpath("a[1]")
-        keyStr = keyElement.get_attribute("data-key")
-        keywords = keyStr.split('/')
-        # print("keywords:", keywords)
+        
+        count2 = 0
+        flag2 = True
+        keywords = []
+        while flag2 and count2 < retry_max:
+            try:
+                keyElement = item.find_element_by_xpath("div[@class='info']/p[@class='info_left left']/a")
+                keyStr = keyElement.get_attribute("data-key")
+                keywords = keyStr.split('/')
+                flag2 = False
+            except:
+                count2 += 1
+                time.sleep(1)
+        if flag2:
+            continue
+
+
+        # keyElement = item.find_element_by_xpath("div[@class='info']/p[@class='info_left left']/a")
+        # keyStr = keyElement.get_attribute("data-key")
+        # keywords = keyStr.split('/')
+
+        print("title:", title)
+        print("keywords:", keywords)
 
         papers[title] = keywords
+    
+    return True
 
+lastPage = ''
 
 def switchNextPage(browser):
-    page = browser.find_element_by_xpath("//div[@class='page']")
-    nextPage = page.find_element_by_xpath("a[text()='下一页>']")
-    if nextPage:
-        nextPage.click()
-        return True
-    else:
+    global lastPage
+    wait = WebDriverWait(browser, 10, 0.5)
+    pageLocator = (By.XPATH, "//div[@class='page']//a[text()='下一页>']")
+    try:
+        wait.until(EC.presence_of_element_located(pageLocator))
+    except:
+        print("switchNextPage failed!")
         return False
 
-    # //*[@id="PageContent"]/div[1]/div[2]/div[13]/a[11]
+    nextPageElement = browser.find_element_by_xpath("//div[@class='page']//a[text()='下一页>']")
+    if nextPageElement:
+        nextPage = nextPageElement.get_attribute('onclick')
+        if lastPage == nextPage:
+            print("No next page, finish!")
+            return False
+        else:
+            lastPage = nextPage
+            nextPageElement.click()
+            return True
+    else:
+        print("Switch to next page failed!")
+        return False
     
 
 
@@ -61,12 +119,15 @@ if __name__ == "__main__":
     openPage(browser)
     papers = {}
     collectTitleAndKeyword(browser, papers)
-
+    time.sleep(2)
+    
     #2、翻页，批量选取链接
-    # while switchNextPage(browser):
-    #     time.sleep(0.1)
-    #     collectTitleAndKeyword(browser, papers)
-    #     time.sleep(1)
+    while switchNextPage(browser):
+        delay = 2+random.random()
+        time.sleep(delay)
+        if not collectTitleAndKeyword(browser, papers):
+            break
+        time.sleep(delay)
         
     browser.quit()
     print("采集了%d条数据"% len(papers))
